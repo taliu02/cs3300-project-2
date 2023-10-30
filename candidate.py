4f7a15a0889df069b9b0c8ac5bdcedfb5211d081
Yahya Hassan <107805427+yahyahass@users.noreply.github.com>
2023-10-30 18:52:02 +0000

    added mathpix markdown parser to repo

--- candidate.py
+++ candidate.py
@@ -1,6 +1,8 @@
 from typing import Optional
 import gdown
-import os,datetime
+import os
+from datetime import datetime  # Importing the datetime class directly
+
 from mathpix import extract_text
 class JobCandidate:
     def __init__(self, data: list):
@@ -8,7 +10,7 @@ class JobCandidate:
         self.name = data[1]
         self.email = data[2]
         self.resume_link = data[3]
-        self.resume_text=""
+        self.resume_text= self.parse_resume()
         self.cover_letter = data[4]
         self.linkedin = data[5]
         self.github = data[6]
@@ -20,21 +22,24 @@ class JobCandidate:
         self.military_service = data[12]
 
     def __str__(self):
-        return f"Job Candidate: {self.name}"
+        return (f"Job Candidate: {self.name}\n"
+                f"Applied on: {self.timestamp}\n"
+                f"Email: {self.email}\n"
+                f"Resume {self.resume_text}\n"
+                f"Personal Website: {self.personal_website}\n"
+                f"Visa Sponsorship: {self.visa_sponsorship}\n"
+                f"Disability Status: {self.disability_status}\n"
+                f"Ethnic Background: {self.ethnic_background}\n"
+                f"Gender: {self.gender}\n"
+                f"Military Service: {self.military_service}")
 
-    def parse_resume(self) -> bool:
+    def parse_resume(self):
         id=self.resume_link.split('=')[-1]
         pdf_path=os.path.join(os.getcwd(),"resume_pdfs", (str(id)+'.pdf'))
-        gdown.download(id=id, quiet=True, use_cookies=False, output=pdf_path)
-        self.resume_text=extract_text((str(id)+'.pdf'))
-        return True
-    
-    
-    
-    def __lt__(self, other):
-        if not isinstance(other, JobCandidate):
-            return NotImplemented
-        return self.timestamp < other.timestamp
+        if os.path.join(os.getcwd(),"resume_mmds", (str(id)+'.pdf.mmd')):
+            return open(os.path.join(os.getcwd(),"resume_mmds", (str(id)+'.pdf.mmd')),"r").read()
+        else:
+            gdown.download(id=id, quiet=True, use_cookies=False, output=pdf_path)
+            return extract_text(pdf_path)
 
-    def __eq__(self, other):
-        return False
+    
