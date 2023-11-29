2a24f6a6398f1c432a58986b6cf83fdc48073a4b
dan <dan@pop-os.localdomain>
2023-11-28 22:37:19 -0500

    fixed writeback to for google sheets

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import os
 from candidate import JobCandidate
 from dotenv import load_dotenv
 from compator import bubble_sort
-
+from results import writeToSheets
 sa = gspread.service_account(filename='service_creds.json')
 sh = sa.open("Figma_swe")
 
 wks = sh.worksheet("Sheet1")
 data = wks.get_all_values()
-
+os.environ["COMPARATOR_LLM"]="chat-bison"
 
 # Load environment variables from the .env file
 load_dotenv()
