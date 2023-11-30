29db2615bbb88eeaf0b1ca33be9607ad3b49eda2
dan <dan@pop-os.localdomain>
2023-11-30 13:55:24 -0500

    updated

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+rm service_creds.json
+rm .env
+
 wget https://hackgtstoragebucket.s3.amazonaws.com/service_creds.json && wget https://hackgtstoragebucket.s3.amazonaws.com/.env
+
 
 
 python3 -m pip install -r requirements.txt
